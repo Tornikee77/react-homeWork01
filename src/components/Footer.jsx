e6cbee6eee6dc55366eb6f9657c4c4591ea97ee4
Tornikee77 <tornikeesitashvili999@gmail.com>
2025-03-16 03:03:51 +0400

    feature/addingHeaderMainFooter
    addingHeaderMainFooter

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <p>© 2024 My Website</p>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+function MainContent() {
+  return (
+    <div>
+      <main>
+        <h2>Main Content</h2>
+        <p>react is better than javascript!</p>
+      </main>
+    </div>
+  );
+}
+export default MainContent;
